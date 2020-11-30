5831730e7ae8831fc8357623bef2b5c7ed7d1666
JohnZF <zhangfeng159@live.com>
2020-11-30 11:36:48 +0800

    Reconstruct Projects.

--- utils/sampling.py
+++ utils/sampling.py
@@ -56,13 +56,25 @@ def sampling_ds(x_sou_ds, y_train, random_state):
     return x_sou_ds_new, y_train_new
 
 
-def over_sampling(samples, sample_indices, random_state):
-    rand_sys = random_state.rand(len(sample_indices))
-    new_samples = []
-    for idx, i in enumerate(sample_indices):
-        sys_sample = samples[i] + rand_sys[idx] * (2 * samples[i] - samples[i - 1] - samples[i + 1])
-        new_samples.append(sys_sample)
-    return np.array(new_samples)
+def random_sampling_ns(x_tar_ns, x_sou_ns, random_state):
+    random_state = check_random_state(random_state)
+    x_sou_ns_new = []
+    num_samples = len(x_tar_ns)
+    for x in x_sou_ns:
+        indices = np.array(range(x.shape[0]))
+        if len(x) == num_samples:
+            x_sou_ns_new.append(x)
+        elif len(x) > num_samples:
+            # under sampling
+            indices = random_state.choice(indices, num_samples)
+            indices = np.sort(indices)
+            x_sou_ns_new.append(_safe_indexing(x, indices))
+        elif len(x) < num_samples:
+            # over sampling
+            indices = random_state.randint(0, len(indices), num_samples)
+            indices = np.sort(indices)
+            x_sou_ns_new.append(_safe_indexing(x, indices))
+    return x_sou_ns_new
 
 
 def random_sampling_ds(x_sou_ds, y_train, random_state=30):
@@ -78,14 +90,13 @@ def random_sampling_ds(x_sou_ds, y_train, random_state=30):
         elif len(x) > num_samples:
             # under sampling
             indices = random_state.choice(indices, num_samples)
+            indices = np.sort(indices)
             x_sou_ds_new.append(_safe_indexing(x, indices))
             y_train_new.append(_safe_indexing(y, indices))
         elif len(x) < num_samples:
             # over sampling
-            sample_indices = random_state.randint(1, len(indices)-1, num_samples - len(x))
-            new_samples = over_sampling(x, sample_indices, random_state)
-            x_sou_ds_new.append(np.vstack((x, new_samples)))
-
-            indices = np.append(indices, sample_indices)
+            indices = random_state.randint(0, len(indices), num_samples)
+            indices = np.sort(indices)
+            x_sou_ds_new.append(_safe_indexing(x, indices))
             y_train_new.append(_safe_indexing(y, indices))
     return x_sou_ds_new, y_train_new
